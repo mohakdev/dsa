4502bf41448ca7bbe4593343bd2288c31176386a
Mohak Jain <mohakj500@gmail.com>
2026-01-23 23:36:57 +0530

    changed question in plus one

--- src/questions/PlusOne.java
+++ src/questions/PlusOne.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class PlusOne {
 
     public static void main(String[] args) {
-        int[] digits = {9, 8, 9};
+        int[] digits = {9, 8, 9, 9};
         System.out.println(Arrays.toString(plusOne(digits)));
     }
 
